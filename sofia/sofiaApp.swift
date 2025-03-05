97746567416cccfb3dd8ff9838887098ff2425a9
João Henrique <joaohenriquess3287@gmail.com>
2025-03-05 09:33:09 -0300

    Initial Commit

---
+++ sofia/sofiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sofiaApp.swift
+//  sofia
+//
+//  Created by Henrique on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct sofiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
